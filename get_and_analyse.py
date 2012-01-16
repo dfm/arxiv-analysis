ebe06f19e70270b544a8fbf397507b9460f58ecd
Dan F-M <dan@danfm.ca>
2012-01-16 13:15:53 -0500

    don't double run

--- get_and_analyse.py
+++ get_and_analyse.py
@@ -8,22 +8,24 @@ from analyse import analyse_listings
 
 s = sched.scheduler(time.time, time.sleep)
 
+def get_time():
+    d = datetime.datetime.now()
+    d += datetime.timedelta(days=1)
+    d = d.replace(hour=21, minute=0, second=0)
+    t = time.mktime(d.timetuple())
+    return t
+
 def do_analysis():
-    strt = time.time()
     tmp, n = get_recent_listings()
     analyse_listings(n=n+1)
 
-    d = datetime.datetime.now()
-    if d.hour == 21:
-        d += datetime.timedelta(days=1)
-    d = d.replace(hour=21, minute=0, second=0)
-    t = time.mktime(d.timetuple())
-
-    s.enterabs(t, 1, do_analysis, ())
+def do_sched():
+    s.enterabs(get_time(), 1, do_analysis, ())
     print "Queue:"
     print "------"
     print s.queue
     s.run()
 
 do_analysis()
+do_sched()
 
