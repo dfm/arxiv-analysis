be3f74bda47b72f85b508c654c3894dcb29f5159
Dan F-M <dan@danfm.ca>
2012-01-16 12:32:22 -0500

    adding scheduler

--- get_and_analyse.py
+++ get_and_analyse.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+import sched
+import time
 from get_recent_listings import get_recent_listings
 from analyse import analyse_listings
 
-tmp, n = get_recent_listings()
-analyse_listings(n=n+1)
+s = sched.scheduler(time.time, time.sleep)
+
+def do_analysis():
+    strt = time.time()
+    tmp, n = get_recent_listings()
+    analyse_listings(n=n+1)
+    s.enter(24*60*60-(time.time()-strt), 1, do_analysis, ())
+    s.run()
+
+do_analysis()
 
