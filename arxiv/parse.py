66e3fb75e315138ee1575118386aaa695dfbc558
Dan F-M <danfm@nyu.edu>
2012-12-01 02:42:32 -0500

    adding parsing script

---
+++ arxiv/parse.py
@@ -0,0 +1,38 @@
+import re
+from datetime import datetime
+import xml.etree.cElementTree as ET
+
+
+record_tag = u".//{http://www.openarchives.org/OAI/2.0/}record"
+ns_re = re.compile(r"\{(?:.*?)\}(.*)")
+date_fmt = u"%a, %d %b %Y %H:%M:%S %Z"
+
+
+def parse(fns):
+    for f in fns:
+        tree = ET.parse(f)
+        root = tree.getroot()
+        for r in root.findall(record_tag):
+            doc = {}
+            for el in r.iter():
+                txt = el.text
+                if txt is None:
+                    for k, v in el.attrib.iteritems():
+                        doc[unicode(k.lower())] = unicode(v)
+                elif txt.strip() != u"":
+                    k = unicode(ns_re.search(el.tag).groups()[0].lower())
+                    txt = unicode(txt.strip())
+                    if k == u"date":
+                        txt = datetime.strptime(txt, date_fmt)
+                    elif k == u"categories":
+                        txt = [c.strip() for c in txt.split(u",")]
+                    doc[k] = txt
+            print doc
+
+
+if __name__ == "__main__":
+    import sys
+    import glob
+
+    fns = glob.glob(sys.argv[1])
+    parse(fns)
