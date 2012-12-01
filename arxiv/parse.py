7aa35265952f86a6515a2f5fef504e421bf7a35b
Dan F-M <danfm@nyu.edu>
2012-12-01 08:24:22 +0000

    parsing script runs

--- arxiv/parse.py
+++ arxiv/parse.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
+
 import os
 import re
 from datetime import datetime
 import xml.etree.cElementTree as ET
+from multiprocessing import Pool
 
 import pymongo
 
@@ -15,32 +18,38 @@ server = os.environ.get(u"MONGO_SERVER", "localhost")
 port = int(os.environ.get(u"MONGO_PORT", 27017))
 
 
-def parse(fns):
+def parse_one(f):
+    print(u"Starting: {0}".format(f))
     db = pymongo.Connection(server, port).arxiv
-    for f in fns:
-        tree = ET.parse(f)
-        root = tree.getroot()
-        for r in root.findall(record_tag):
-            doc = {}
-            for el in r.iter():
-                txt = el.text
-                if txt is None:
-                    for k, v in el.attrib.iteritems():
-                        doc[unicode(k.lower())] = unicode(v)
-                elif txt.strip() != u"":
-                    k = unicode(ns_re.search(el.tag).groups()[0].lower())
-                    txt = unicode(txt.strip())
-                    if k == u"date":
-                        txt = datetime.strptime(txt, date_fmt)
-                    elif k == u"categories":
-                        txt = [c.strip() for c in txt.split(u",")]
-                    doc[k] = txt
-            print doc
+    coll = db.abstracts
+
+    tree = ET.parse(f)
+    root = tree.getroot()
+    for i, r in enumerate(root.findall(record_tag)):
+        doc = {}
+        for el in r.iter():
+            txt = el.text
+            if txt is None:
+                for k, v in el.attrib.iteritems():
+                    doc[unicode(k.lower())] = unicode(v)
+            elif txt.strip() != u"":
+                k = unicode(ns_re.search(el.tag).groups()[0].lower())
+                txt = unicode(txt.strip())
+                if k == u"date":
+                    txt = datetime.strptime(txt, date_fmt)
+                elif k == u"categories":
+                    txt = [c.strip() for c in txt.split()]
+                doc[k] = txt
+        coll.insert(doc)
+    print(u"Finished {0}".format(f))
+
+
+def parse(fns):
+    p = Pool()
+    p.map(parse_one, list(fns))
 
 
 if __name__ == "__main__":
     import sys
-    import glob
 
-    fns = glob.glob(sys.argv[1])
-    parse(fns)
+    parse(sys.argv[1:])
